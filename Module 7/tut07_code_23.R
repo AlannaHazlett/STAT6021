dcb4051856dad1f95b3ce5e2c9f2dfaa4262bb1e
AlannaHazlett <alanna.lohff@gmail.com>
2025-01-26 18:02:16 -0500

    Module 7 upload

---
+++ Module 7/tut07_code_23.R
@@ -0,0 +1,35 @@
+Data<-read.table("nfl.txt", header=TRUE)
+
+round(cor(Data),3)
+
+##fit MLR
+result<-lm(y~x1+x5+x7+x8, data=Data)
+summary(result)
+
+reduced<-lm(y~x8, data=Data)
+
+##general linear F test to compare reduced model with full model
+anova(reduced, result)
+
+##approach 2
+anova(result) ##output doesn't give us needed info, because the predictors are in a bad 
+#order. We must put the predictor we want to keep at the beginning and the potential 
+#drop predictors at the end.
+
+##rearrange. put predictors to drop last in lm()
+full<-lm(y~x8+x1+x5+x7, data=Data)
+
+anova(full)
+
+##fit MLR with all predictors
+all<-lm(y~., data=Data)
+
+##look at t tests, and F test
+summary(all)
+
+##correlation matrix, round to 3 decimal
+round(cor(Data[,-1]),3)
+
+##VIFs
+library(faraway)
+faraway::vif(all)
